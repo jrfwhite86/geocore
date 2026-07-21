e3ab290e13d4a518c6ff8135ecba93119808e5af
Jonathan White <37590879+jrfwhite86@users.noreply.github.com>
2026-07-21 09:22:55 +0100

    Delete python/src/geocore_connect directory

--- python/src/geocore_connect/__init__.py
+++
@@ -1,8 +0,0 @@
-"""geodb_connect: IAM-authenticated connectivity toolkit for the geodb RDS PostgreSQL instance."""
-
-from .auth import get_auth_token
-from .config import DbConfig
-from .db import connect
-
-__all__ = ["DbConfig", "get_auth_token", "connect"]
-
